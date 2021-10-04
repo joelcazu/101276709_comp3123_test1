89a0a84009e0f41265d17f347b1e9fc3b20d2382
joelcazu <61336088+joelcazu@users.noreply.github.com>
2021-10-04 18:54:25 -0400

    ES6 Features

--- LAB-TEST-1/question-1/script.js
+++ LAB-TEST-1/question-1/script.js
@@ -1,1 +1,18 @@
-const lowerCaseWords 
+const lowerCaseWords = (mixedArray) => {
+    return new Promise((resolve, reject) => {
+      if (mixedArray.length >= 0 && Array.isArray(mixedArray)) {
+        const filterArStr = mixedArray.filter((itemArray) => typeof itemArray === "string");
+        const mapArLoCaseStr = filterArStr.map((itemFilterAr) =>
+        itemFilterAr.toLowerCase()
+        );
+        resolve(mapArLoCaseStr);
+      } else {
+        reject("Error");}});
+  };
+  
+  const mixedArray = ["PIZZA", 10, true, 25, false, "Wings"];
+  lowerCaseWords(mixedArray)
+    .then((value) =>
+      console.log(value) )
+    .catch((error) =>
+      console.log(error));
